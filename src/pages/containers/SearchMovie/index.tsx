bc90e489b14199b3b955364512e9510341485c34
Nelson Acosta <nelsonacosta@Nelsons-MacBook-Pro.local>
2020-04-26 22:20:26 -0400

    improving movie component

--- src/pages/containers/SearchMovie/index.tsx
+++ src/pages/containers/SearchMovie/index.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {Search} from '../../components/Search'
-import {peliculas} from '../../../api/dataToSearch.json' 
-import { Movie } from './../../components/Movie'
+import {peliculas} from '../../../api/dataToSearch.json'
+// import Movie  from './../../components/Movie'
+import Movie  from './../../components/Movie'
 
 export default class SearchMovie extends Component<any, any> {
   state={
@@ -21,12 +22,12 @@ export default class SearchMovie extends Component<any, any> {
         </div>
         <div style={{backgroundColor:'#131722', height:'100%', display:'flex',flexWrap:'wrap', padding:'0em 8em'}}>
         {
-        this.state.data.length > 0 
+        this.state.data.length > 0
           ? this.state.data.map(item => <div style={{width:'25%', display:'flex', justifyContent:'center', padding:'1em 0em'}}>
           <Movie {...item}/>
         </div>
         )
-        : 
+        :
           <h1 style={{color:'white', textAlign:'center'}}>No se encontraron datos {'😅'}</h1>
       }
         </div>
